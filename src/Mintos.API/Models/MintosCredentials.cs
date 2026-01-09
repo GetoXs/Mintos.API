ec375e3a7bbb3fac3d48f8c817f612df0bfaf5bd
Mateusz Przybyłek <getoxs@gmail.com>
2026-01-09 11:00:32 +0100

    Add IsNull and IsValid methods to MintosCredentials for improved validation of session and CSRF token properties.

--- src/Mintos.API/Models/MintosCredentials.cs
+++ src/Mintos.API/Models/MintosCredentials.cs
@@ -5,5 +5,12 @@ public class MintosCredentials
     public string? MintosMwSessionId { get; set; }
     public string? MintosPhpSessionId { get; set; }
     public string? MintosAntiCsrfToken { get; set; }
+
+    public bool IsNull() => 
+        string.IsNullOrEmpty(MintosMwSessionId) || 
+        string.IsNullOrEmpty(MintosPhpSessionId) || 
+        string.IsNullOrEmpty(MintosAntiCsrfToken);
+
+    public bool IsValid() => !IsNull();
 }
 
